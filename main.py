68aff66c0b65b801c36539ba45e43b325902b804
unknown <krystian.smolski@gmail.com>
2021-04-14 22:43:05 +0200

    Not finished Flask REST API

--- main.py
+++ main.py
@@ -1,49 +1,65 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_restful import Api, Resource, reqparse
 import requests
 import json
+import collections
 
 app = Flask(__name__)
 api = Api(app)
 
-# book_put_args = reqparse.RequestParser()
-# book_put_args.add_argument("name")
+
 dataset = requests.get('https://www.googleapis.com/books/v1/volumes?q=Hobbit')
-result = {}
 
-# for data in dataset.json()['items']:
-#     title = data['volumeInfo']['title']
-#     result.append(title)
-    # print (type(data['volumeInfo']['title']))
-#print(sorted(result))
 
-for data in dataset.json()['items']:
-    title = data['volumeInfo']['title']
-    result['title'] = title
-    # print(title)
-
-print(result)
-
-# print(result)
-# title_dict = dict.fromkeys(result, "title")
-# print(title_dict)
-# title_dict = {v: k for k, v in title_dict.items()}
-# print(title_dict)
-
-@app.route('/')
-def home():
-    return render_template('rectuitment.html')
-
-@app.route('/book')
+@app.route('/book', methods=['GET'])
 def GetAllBooks():
+    published_date = request.args.get('published_date')
+    sort = request.args.get('sort')
+    authors = request.args.get('authors').encode()
+    if (published_date):
+        result = []
+        for data in dataset.json()['items']:
+            if published_date == data['volumeInfo']['publishedDate']:
+                result.append(data['volumeInfo']['title'])
+        return jsonify({'books' : result})        
+
+    elif (sort):
+        result = {}
+        titles_list = []
+        published_date_list = []
+        for data in dataset.json()['items']:
+            titles_list.append(data['volumeInfo']['title'])
+            published_date_list.append(data['volumeInfo']['publishedDate'])
+            pair_lists = zip(titles_list, published_date)
+            result_dict = dict(pair_lists)
+            result = collections.OrderedDict(sorted(result_dict.items()))
+        return jsonify({ "books" : result})
+
+
+    elif (authors):
+        result = []
+        for data in dataset.json()['items']:
+            string_data = str(data['volumeInfo']['authors'])
+            if authors == string_data.encode():
+                result.append(data['volumeInfo']['title'])
+        return jsonify({ "books" : result})
+
+    else:
+        result = []
+        for data in dataset.json()['items']:
+            result.append(data['volumeInfo']['title'])
+        return jsonify({ "books" : result})
+
+
+@app.route('/book/<id>', methods=['GET'])
+def GetBookById(id):
+    result = {}
     for data in dataset.json()['items']:
-        title = data['volumeInfo']['title']
-        result.append(title)
-        title_dict = dict.fromkeys(result, "title")
-        title_dict = {v: k for k, v in title_dict.items()}
-    print(sorted(title_dict))
-    return 0 #jsonify("title" : [result])
+        if data['id'] == id:
+            result["data"] = data["volumeInfo"]
+    return jsonify( result )
 
+        
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
